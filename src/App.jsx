d1cd9c5af63ddd075e400f14b00488bf57ff63cd
s0-p <b00v0429@gmail.com>
2026-07-09 17:07:21 +0900

    feat: 선택된 항목 표시

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import './App.css';
 import data from './data/data.json';
 import StudyInfo from './components/StudyInfo';
 import StudyList from './components/StudyList';
+import { useState } from 'react';
 
 function App() {
   console.log(data);
+  const [selectedId, setSelectedId] = useState(null);
   return (
     <>
       <h1>React Basic Review Mission 2</h1>
       <p>전체 학습 항목 수 : {data.length}</p>
       <StudyInfo title={data[0].title} desc={data[0].desc} category={data[0].category} />
-      <StudyList items={data} />
+      <StudyList
+        items={data}
+        selectedId={selectedId}
+        onSelect={(_selectedId) => {
+          setSelectedId(_selectedId);
+        }}
+      />
     </>
   );
 }
--- src/components/StudyList.jsx
+++ src/components/StudyList.jsx
@@ -1,11 +1,20 @@
-export default function StudyList({ items }) {
-  const dataList = items.map((item) => (
-    <li key={item.id}>
-      <h3>{item.title}</h3>
-      <p>{item.desc}</p>
-      <p>{item.category}</p>
+export default function StudyList({ items, selectedId, onSelect }) {
+  const dataList = items.map((item, index) => (
+    <li
+      key={item.id}
+      className={`card text-center ${selectedId === item.id ? 'active' : ''}`}
+      onClick={() => {
+        onSelect(item.id);
+      }}
+    >
+      <div className="card-body">
+        <h5 className="card-title">{`${index + 1}. ${item.title}`}</h5>
+        <p className="card-text">{item.desc}</p>
+        <p className="card-text">분류 : {item.category}</p>
+        {selectedId === item.id && <p>선택된 항목입니다.</p>}
+      </div>
     </li>
   ));
   console.log(dataList);
-  return <>{dataList}</>;
+  return <ul>{dataList}</ul>;
 }
